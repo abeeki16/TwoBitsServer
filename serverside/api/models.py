dca02110bd5153eaa562d3de1385b9e7dcec9fdf
Camilla Satte <camillasatte@gmail.com>
2019-11-14 23:32:41 -0500

    categroies and charity models added

--- serverside/api/models.py
+++ serverside/api/models.py
@@ -8,3 +8,19 @@ class Profile(models.Model):
     last_name = models.CharField(max_length=64)
     date_joined = models.DateField(auto_now_add=True)
 
+
+class Charity(models.Model):
+    charity_name = models.CharField(max_length=128)
+    description = models.TextField()
+    charity_contact_name = models.CharField(max_length=64)
+    charity_contact_email = models.CharField(max_length = 128)
+    charity_contact_phone = models.CharField(max_length = 64)
+    charity_category = models.CharField(max_length=128)
+    charity_donators = models.ManyToManyField(Profile)
+
+
+class Category(models.Model):
+    category_name = models.CharField(max_length=128)
+    category_charities = models.ManyToManyField(Charity)
+    category_donators = models.ManyToManyField(Profile)
+
